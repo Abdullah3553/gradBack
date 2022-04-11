86ce031fdc95de3031538fc0a6443ae69fa70ec4
AseelElsohly <aseelalsihly@gmail.com>
2022-04-11 21:45:26 +0200

    AUTHENTICATION_METHOD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { Service } from './service';
+import { AuthenticationMethodModule } from './authentication_method/authentication_method.module';
 
 @Module({
-  imports: [],
+  imports: [AuthenticationMethodModule],
   controllers: [AppController],
   providers: [Service],
 })
