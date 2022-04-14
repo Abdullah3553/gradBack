d5a8546b9d41d6f32d7db54834dd758273818dfc
Abdullah <abdullah.ayoub2000@gmail.com>
2022-04-14 12:02:03 +0200

    user module init

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,27 +7,27 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
+  @Post('/new')
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
-  @Get()
+  @Get('/find/all')
   findAll() {
     return this.userService.findAll();
   }
 
-  @Get(':id')
+  @Get('/find/:id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Patch('/update/:id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
-  @Delete(':id')
+  @Delete('/delete/:id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
   }
