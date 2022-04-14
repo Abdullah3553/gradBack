462bf942b2a3c0aa8f1db00ec40a56a6fc7a8cdb
Abdullah <abdullah.ayoub2000@gmail.com>
2022-04-14 12:21:00 +0200

    create user dto + general validation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,3 +1,20 @@
-export class CreateUserDto {
+import {IsDate, IsEmail, IsNotEmpty} from "class-validator";
 
+export class CreateUserDto {
+    @IsNotEmpty()
+    username:string
+
+    @IsNotEmpty()
+    @IsDate()
+    birth_Date:string
+
+    @IsNotEmpty()
+    @IsEmail()
+    email:string
+
+    country?:string
+
+    street?:string
+
+    city?:string
 }
