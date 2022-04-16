2bcc8c3dc65d2e80d16de456773f9ad26ea267a2
lamees962 <66960794+lamees962@users.noreply.github.com>
2022-04-16 03:55:36 +0200

    general validation for authenticators

--- src/authenticator/dto/create-authenticator.dto.ts
+++ src/authenticator/dto/create-authenticator.dto.ts
@@ -1,5 +1,14 @@
+import { IsString, IsInt, IsNotEmpty, IsPositive } from 'class-validator';
+
 export class CreateAuthenticatorDto {
+  @IsString()
+  @IsNotEmpty()
   signature: string;
+  @IsInt()
+  @IsNotEmpty()
+  @IsPositive()
   priority: number;
+  @IsString()
+  @IsNotEmpty()
   access_token: string;
 }
