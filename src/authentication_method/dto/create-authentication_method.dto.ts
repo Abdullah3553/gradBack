72dafcca6c8591426913102618df9447ba2a252c
Abdullah <abdullah.ayoub2000@gmail.com>
2022-04-20 11:27:57 +0200

    TODO..

--- src/authentication_method/dto/create-authentication_method.dto.ts
+++ src/authentication_method/dto/create-authentication_method.dto.ts
@@ -2,7 +2,7 @@ import { IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateAuthenticationMethodDto {
   @IsInt()
-  id: number;
+  id: number; // TODO Aseel
   @IsNotEmpty()
   @IsString()
   title: string;
