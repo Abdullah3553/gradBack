71cc5e611c1adcd887b41e0744f8273d2e74e8cc
AseelElsohly <aseelalsihly@gmail.com>
2022-04-26 20:55:57 +0200

    update token service

--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -2,11 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { CreateTokenDto } from './dto/create-token.dto';
 import { UpdateTokenDto } from './dto/update-token.dto';
 import {PrismaService} from "../prisma/prisma.service";
+import crypto from "crypto";
 const { hashToken } = require('../../utils/hashToken');
+const jwt = require('jsonwebtoken');
+
+
 
 @Injectable()
 export class TokenService {
+
   constructor(private prisma: PrismaService) {}
+
+  private hashToken(token) {
+    return crypto.createHash('sha256').update(token).digest('hex');
+  }
+
   create(userId: number) {
     const token = "token should be here"
     const user = this.prisma.token.create({
@@ -52,6 +62,55 @@ export class TokenService {
     })
     return token
   }
+  async deletetoken(id) {
+  return await this.prisma.token.update({
+    where: {
+      id,
+    },
+    data: {
+      revoked: true
+    }
+  });
+}
+
+async revokeTokens(userId) {
+  return this.prisma.token.updateMany({
+    where: {
+      userId
+    },
+    data: {
+      revoked: true
+    }
+  });
+}
+
+
+// Usually I keep the token between 5 minutes - 15 minutes
+  private generateAccessToken(userId) {
+    return jwt.sign({ userId: userId }, process.env.JWT_ACCESS_SECRET, {
+      expiresIn: '5m',
+    });
+  }
+  private generateRefreshToken(user, jti) {
+    return jwt.sign({
+      userId: user.id,
+      jti
+    }, process.env.JWT_REFRESH_SECRET, {
+      expiresIn: '8h',
+    });
+  }
+
+  private generateTokens(user, jti) {
+    const accessToken = this.generateAccessToken(user);
+    const refreshToken = this.generateRefreshToken(user, jti);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+}
+
 
 // used when we create a refresh token.
 // async addtokenToWhitelist({ jti, token, userId }) {
@@ -73,28 +132,4 @@ export class TokenService {
 //   });
 // }
 //
-// // soft delete tokens after usage.
-//  async deletetoken(id) {
-//   return this.prisma.token.update({
-//     where: {
-//       id,
-//     },
-//     data: {
-//       revoked: true
-//     }
-//   });
-// }
-//
-// async revokeTokens(userId) {
-//   return this.prisma.token.updateMany({
-//     where: {
-//       userId
-//     },
-//     data: {
-//       revoked: true
-//     }
-//   });
-// }
-}
-
 
