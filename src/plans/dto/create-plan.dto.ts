3e9bd5becb7a52b88476f089fec69e53c51a0a30
Abdullah <abdullah.ayoub2000@gmail.com>
2022-04-27 15:47:20 +0200

    plans create dto

--- src/plans/dto/create-plan.dto.ts
+++ src/plans/dto/create-plan.dto.ts
@@ -1,1 +1,20 @@
-export class CreatePlanDto {}
+import {IsBoolean, IsIn, IsNotEmpty, IsNumber, IsString} from "class-validator";
+
+export class CreatePlanDto {
+    @IsNotEmpty()
+    @IsString()
+    name:string
+
+    description:string
+
+    @IsNotEmpty()
+    @IsNumber()
+    price:number
+
+    @IsNotEmpty()
+    @IsNumber()
+    duration_months:number
+
+    @IsBoolean()
+    activated:boolean
+}
