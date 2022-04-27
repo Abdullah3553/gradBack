442ee99a157d2056a9840fd378908cd17bb88301
Abdullah <abdullah.ayoub2000@gmail.com>
2022-04-27 16:01:26 +0200

    plans service CRUD implemented

--- src/plans/plans.service.ts
+++ src/plans/plans.service.ts
@@ -1,26 +1,62 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePlanDto } from './dto/create-plan.dto';
 import { UpdatePlanDto } from './dto/update-plan.dto';
+import {PrismaService} from "../prisma/prisma.service";
 
 @Injectable()
 export class PlansService {
-  create(createPlanDto: CreatePlanDto) {
-    return 'This action adds a new plan';
+  constructor(private readonly prisma:PrismaService) {}
+  async create(createPlanDto: CreatePlanDto) {
+    const plan = await this.prisma.plan.create({
+      data:{
+        name:createPlanDto.name,
+        description:createPlanDto.description,
+        price:createPlanDto.price,
+        duration_months:createPlanDto.duration_months,
+        activated:createPlanDto.activated
+      }
+    })
+
+    return plan;
   }
 
-  findAll() {
-    return `This action returns all plans`;
+  async findAll() {
+    const plans = await this.prisma.plan.findMany()
+    return plans
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} plan`;
+  async findOne(id: number) {
+    const plan = await this.prisma.plan.findUnique({
+      where:{id:id}
+    })
+    return plan
   }
 
-  update(id: number, updatePlanDto: UpdatePlanDto) {
-    return `This action updates a #${id} plan`;
+  async update(id: number, updatePlanDto: UpdatePlanDto) {
+    const plan = await this.prisma.plan.update({
+      where:{
+        id:id
+      },
+      data:{
+        name:updatePlanDto.name,
+        description:updatePlanDto.description,
+        price:updatePlanDto.price,
+        duration_months:updatePlanDto.duration_months,
+        activated:updatePlanDto.activated
+      },
+
+    })
+
+    return plan ;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} plan`;
+  async remove(id: number) {
+    const plan = await this.prisma.plan.delete({
+      where:{
+        id:id
+      }
+    })
+    return plan
+
   }
 }
