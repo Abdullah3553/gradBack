350a1ff1c516df2d876ac91d1621cc819ae09d9e
abdullah3553 <abdullah.ayoub2000@gmail.com>
2022-05-14 15:24:23 +0200

    commenting testing objects

--- src/authentication_method/authentication-method-selector.service.ts
+++ src/authentication_method/authentication-method-selector.service.ts
@@ -8,8 +8,8 @@ export class AuthenticationMethodSelectorService {
     ) {}
     methodSelector(authentication_method, username:string, storedSignature:string, sentSignature:string){
         switch (authentication_method.title) {
-            case 'lol' :/*for testing*/
-                return this.passwordMethod.Compare(username, storedSignature, sentSignature)/*for testing*/
+            // case 'lol' :/*for testing*/
+            //     return this.passwordMethod.Compare(username, storedSignature, sentSignature)/*for testing*/
             case 'password' :
                 return this.passwordMethod.Compare(username, storedSignature, sentSignature)
             case 'face_recognition':
