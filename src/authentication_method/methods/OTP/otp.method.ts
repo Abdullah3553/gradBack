70ed6a3f82927ee5fef4c42e17f3a18fa6e1784a
abdullah3553 <abdullah.ayoub2000@gmail.com>
2022-05-14 16:14:44 +0200

    otp and qr method files added

---
+++ src/authentication_method/methods/OTP/otp.method.ts
@@ -0,0 +1,16 @@
+import {createHash} from "crypto";
+import {BaseMethod} from "../base.method";
+import {PrismaService} from "../../../prisma/prisma.service";
+
+export class OtpMethod implements BaseMethod{
+    constructor(private readonly prisma:PrismaService) {}
+    compare(username:string, hashedStoredSignature:string, sentSignature:string, filePath:string){
+        const hashedSentSignature = createHash('sha256').update(sentSignature).digest('hex');
+        if(hashedStoredSignature === hashedSentSignature)return true
+        // const tmp = createHash('sha256').update(hashedStoredSignature).digest('hex')/*for testing*/
+        // if(hashedSentSignature === tmp)return true/*for testing*/
+        return false
+    }
+
+
+}
---
+++ src/authentication_method/methods/QR/qr.method.ts
@@ -0,0 +1,12 @@
+import {createHash} from "crypto";
+import {BaseMethod} from "../base.method";
+
+export class QrMethod implements BaseMethod{
+    compare(username:string, hashedStoredSignature:string, sentSignature:string, filePath:string){
+        const hashedSentSignature = createHash('sha256').update(sentSignature).digest('hex');
+        if(hashedStoredSignature === hashedSentSignature)return true
+        // const tmp = createHash('sha256').update(hashedStoredSignature).digest('hex')/*for testing*/
+        // if(hashedSentSignature === tmp)return true/*for testing*/
+        return false
+    }
+}
