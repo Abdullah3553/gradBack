c0cdd6ac7d8996795adfcf851fcc66218fd8a575
abdullah3553 <abdullah.ayoub2000@gmail.com>
2022-05-24 03:06:15 +0200

    fingerprint code back to work

--- src/authentication_method/methods/Fingerprint/fingerprint.method.ts
+++ src/authentication_method/methods/Fingerprint/fingerprint.method.ts
@@ -5,64 +5,62 @@ import * as path from "path";
 import {AuthenticatorService} from "../../../authenticator/authenticator.service";
 import {BadRequestException, Injectable} from "@nestjs/common";
 import {PrismaService} from "../../../prisma/prisma.service";
-import {retry} from "rxjs";
 const SerialPort = require("serialport").SerialPort;
 const Readline = require('@serialport/parser-readline').ReadlineParser;
-// let arduinoSerialPort = new SerialPort({
-//     path:'COM3',
-//     baudRate: 9600});
+let arduinoSerialPort
 @Injectable()
 export class FingerprintMethod implements BaseMethod{
-    constructor(private readonly prisma:PrismaService,
-                ) {
+    constructor(private readonly prisma:PrismaService,) {
+        try{
+            const com = ''
+            // const com = 'COM3'
+            arduinoSerialPort = new SerialPort({path:com, baudRate: 9600});
+        }catch (err){
+            console.log(err)
+        }
     }
     compare(encryptedStoredSignature:string, sentSignature:string , filePath:string){
-        // encryptedStoredSignature is the file path for the known face image
-        // sentSignature is the file path for the unknown face images
-        //1) Decrypt the encryptedStoredSignature
-        //2) execute the python module with command line
-        //3) return formatted response
         return {
 
         }
     }
-    //
-    //  takeInput(){
-    //     const response={
-    //         valid:false,
-    //         data:''
-    //     }
-    //     return new Promise((resolve,reject)=>{
-    //         if(arduinoSerialPort.isOpen){
-    //             const parser = arduinoSerialPort.pipe(new Readline({ delimiter: '\r' }));// Read the port data
-    //             arduinoSerialPort.write('o', ()=>{
-    //                 parser.on('data', (data)=>{
-    //                     response.data=data;
-    //                         response.valid=true
-    //                         resolve(response)
-    //                 })
-    //             })
-    //         }
-    //     })
-    // }
-    //   async enrollFinger(id:number) {
-    //     const response = {
-    //         valid:false,
-    //         data:''
-    //     }
-    //     return new Promise((resolve, reject)=>{
-    //         if(arduinoSerialPort.isOpen){
-    //             const parser = arduinoSerialPort.pipe(new Readline({ delimiter: '\n' }));// Read the port data
-    //             arduinoSerialPort.write(`e${id}`, ()=>{
-    //                 parser.on('data', (data, err)=>{
-    //                     if(err) resolve(response)
-    //                     response.valid = true
-    //                     response.data = data
-    //                     resolve(response)
-    //                 })
-    //             })
-    //
-    //         }
-    //     })
-    // }
+
+     takeInput(){
+        const response={
+            valid:false,
+            data:''
+        }
+        return new Promise((resolve,reject)=>{
+            if(arduinoSerialPort.isOpen){
+                const parser = arduinoSerialPort.pipe(new Readline({ delimiter: '\r' }));// Read the port data
+                arduinoSerialPort.write('o', ()=>{
+                    parser.on('data', (data)=>{
+                        response.data=data;
+                            response.valid=true
+                            resolve(response)
+                    })
+                })
+            }
+        })
+    }
+      async enrollFinger(id:number) {
+        const response = {
+            valid:false,
+            data:''
+        }
+        return new Promise((resolve, reject)=>{
+            if(arduinoSerialPort.isOpen){
+                const parser = arduinoSerialPort.pipe(new Readline({ delimiter: '\n' }));// Read the port data
+                arduinoSerialPort.write(`e${id}`, ()=>{
+                    parser.on('data', (data, err)=>{
+                        if(err) resolve(response)
+                        response.valid = true
+                        response.data = data
+                        resolve(response)
+                    })
+                })
+
+            }
+        })
+    }
 }
